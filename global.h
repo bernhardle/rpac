588443cf86b06462e4f423c530b4e4354515e0b9
Bernhard <bernhardle@gmx.de>
2024-02-01 21:19:30 +0100

    Moved forward

--- global.h
+++ global.h
@@ -18,7 +18,7 @@
 typedef class loggerCBs loggerCBs_t ;
 //
 #ifdef ARDUINO_UBLOX_NINA_W10
-enum struct rpacPin {logger = 15, button = 27, flow = 33, pressure = A0, pulser = 10, relais = 23, signal = LED_BLUE} ;
+enum struct rpacPin {logger = 15, button = 27, flow = 33, pressure = A0, pulser = 10, relais = LED_RED, signal = LED_BLUE} ;
 #else
 enum struct rpacPin {logger = 15, button= 16, flow = 7, pressure = A0, pulser = 10, relais = 2, signal = LED_BUILTIN} ;
 #endif
